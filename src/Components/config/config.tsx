35b46ecf7d2fcfa0847f9383287d82df7274e0d1
MateuszKretkowski <mkretkowski13@gmail.com>
2024-03-27 13:36:55 +0100

    Envoirmental Variables

--- src/Components/config/config.tsx
+++ src/Components/config/config.tsx
@@ -35,15 +35,14 @@ import { firestore } from "firebase-admin";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKSENADRJbScBhkqtj0lafN6nVx5bXfVI",
-  authDomain: "jesusisking-fb576.firebaseapp.com",
-  databaseURL:
-    "https://jesusisking-fb576-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "jesusisking-fb576",
-  storageBucket: "jesusisking-fb576.appspot.com",
-  messagingSenderId: "402878700894",
-  appId: "1:402878700894:web:dd7941f0b1b151a281c3cf",
-  measurementId: "G-E027LNF97N",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
