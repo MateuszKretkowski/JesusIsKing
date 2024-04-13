b40ab43062715d617073be9cdce639ac67376daa
MateuszKretkowski <mkretkowski13@gmail.com>
2024-04-13 12:55:20 +0200

    Profile Picture (Animations, DONE)

--- src/Components/Forum/ProfilePicture.tsx
+++ src/Components/Forum/ProfilePicture.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./ProfilePicture.css";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { getStorage, ref } from "firebase/storage";
 import { auth, db, upload } from "../config/config";
 import {
@@ -25,6 +25,7 @@ function ProfilePicture({
   const storageRef = ref(storage);
   const [photo, setPhoto] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [isUploadVisible, setIsUploadVisible] = useState(false);
   const [photoURL, setPhotoURL] = useState(
     "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
   );
@@ -32,6 +33,7 @@ function ProfilePicture({
     if (e.target.files[0]) {
       setPhoto(e.target.files[0]);
       console.log("Photo selected.", e.target.files[0]);
+      setIsUploadVisible(true);
     } else {
       console.error("No file selected.");
     }
@@ -45,6 +47,7 @@ function ProfilePicture({
     console.log("Uploading photo...");
 
     setLoading(true);
+    controls.start("uploaded");
     upload(photo, email)
       .then(() => {
         setLoading(false);
@@ -78,6 +81,32 @@ function ProfilePicture({
     setPhoto();
   }, [handleClick]);
 
+  const controls = useAnimation();
+  const uploadVariants = {
+    hidden: {
+      opacity: 0,
+      y: -100
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    uploaded: {
+      opacity: 0,
+      y: 100,
+      transition: {
+        duration: 0.5,
+        type: "spring"
+      },
+    }
+  }
+  useEffect(() => {
+    isUploadVisible ? controls.start("visible") : controls.start("hidden");
+  }, [isUploadVisible])
+
   return (
     <motion.div className="ProfilePicture-wrapper">
         {isAbleToChange && (
@@ -87,13 +116,21 @@ function ProfilePicture({
               className="avatar_input"
             />
         )}
-        <img src={photoURL} alt="Avatar" className="avatar" />
+          <img src={photoURL} alt="Avatar" className="avatar" />
+        {isAbleToChange && (
+            <motion.h2
+              className="avatar_edit"
+            >EDIT</motion.h2>
+        )}
       {isAbleToChange && (
         <motion.div className="avatar_upload-wrapper">
           <motion.button
             disabled={!photo}
             onClick={handleClick}
             className="avatar_upload"
+            variants={uploadVariants}
+            initial={controls}
+            animate={controls}
           >
             UPLOAD
           </motion.button>
